82e99e1af922ff009619c152c8605e4e09227b94
Ray.Kim <ray.kim@themedium.io>
2023-01-11 15:02:51 +0900

    feat: 비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
+// Heroko 에서 가져오는 설정 값
--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true})) // application/x-www-form-urlencoded 분석
+app.use(bodyParser.json()); // application/json 분석
 
 const mongoose = require('mongoose')
-const dbUrl = 'mongodb+srv://medium:medium1234@boilder-plate.kbhvbye.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(dbUrl, {
+mongoose.set('strictQuery', true)
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-//
 
 app.get('/', (req, res) => {
     res.send('Hello World! 안녕하세요!')
+})
+
+app.post('/register', (req, res) => {
+    // 회원가입시 필요한 정보들을 client 에서 가져오면 DB에 저장한다
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
