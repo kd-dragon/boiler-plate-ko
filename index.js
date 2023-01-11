302766501cc351780214570cc42b0af5f5d763c3
Ray.Kim <ray.kim@themedium.io>
2023-01-11 15:57:00 +0900

    feat: bcrypt 비밀번호 암호화

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
     // 회원가입시 필요한 정보들을 client 에서 가져오면 DB에 저장한다
     const user = new User(req.body)
-    user.save((err, doc) => {
+
+    user.save((err, userInfo) => {
         if(err) return res.json({success:false, err})
         return res.status(200).json({
             success: true
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name : {
@@ -30,6 +32,33 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+// 저장하기전 기능 설정
+userSchema.pre('save', function(next){
+    let user = this; // 해당 스키마 인스턴스를 가져온다.
+
+    // 변경이 일어날때마다 비밀번호를 암호화 할 수 있으므로 비밀번호가 바뀔때만 동작되도록 조건문 선언
+    if(user.isModified('password')) {
+        console.log(`######### DEBUGGING 1 #########`)
+        // 비밀번호를 암호화 시킨다
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            console.log(`######### DEBUGGING 2 #########`)
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                console.log(`######### DEBUGGING 3 #########`)
+                if (err) return next(err)
+                user.password = hash // 암호화 성공시 hash 된 비밀번호로 교체
+                next()
+            })
+        })
+        console.log(`######### DEBUGGING 4 #########`)
+    } else {
+        next()
+    }
+    console.log(`######### DEBUGGING 5 #########`)
+})
+// *주의) 비동기 호출로 인해 실제 수행순서는 1-4-5-2-3
 
 // 스키마를 모델로 감싸준다.
 const User = mongoose.model('User', userSchema)
